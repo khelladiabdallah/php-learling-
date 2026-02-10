4cabbd2ca8e44098c749740d364cbac51fa6928a
khelladiabdallah <abdallahbusiness2024@gmail.com>
2026-02-10 15:53:39 +0100

    for loop and if condition

--- inedx.php
+++ inedx.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    khelladi abdallah 
-    <?php echo khelladi abdallah ?>
-</body>
-</html>
+<?php
+
+for($i = 0 ; $i < 10 ; $i ++ ){
+
+    echo "khelladi abdlah       ";
+    if (($i % 2)== 0){
+        echo "<br>";
+    }
+}
