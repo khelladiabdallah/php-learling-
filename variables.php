a869c4e5946161595139c6e9e2d28d8c71c48da0
khelladiabdallah <abdallahbusiness2024@gmail.com>
2026-02-21 03:01:51 +0100

    add cous

---
+++ variables.php
@@ -0,0 +1,6 @@
+<?php
+$username ="abdallah";// les noms ave  username pour le code etre claire 
+$$username="khelladi ";// variable de variable elle déconséille pour la progrmation moderne 
+echo $username ;
+echo "<br>";
+echo $$username ; // cours 17 a revoir 
